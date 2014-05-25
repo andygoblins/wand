44d0105ff60a73fbf98f1333b111387e3cc7b3f2
Andrew <thisismyfriendsemailaddress@gmail.com>
2014-05-25 12:43:59 -0500

    a blank slate for cmd language experiment

---
+++ wand.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import sys
+from gi.repository import Gtk, GtkSource, GObject
+
+class Wand():
+    def __init__(self, filename):
+        self.window = Gtk.Window()
+        self.window.connect('delete-event', Gtk.main_quit)
+
+        self.box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+        self.cmd = cmdBox()
+        self.box.pack_start(self.cmd, False, False, 0)
+
+        self.filename = filename
+        try:
+            with open(filename, 'r') as file:
+                buf = file.read()
+        except FileNotFoundError:
+            buf = ''
+            filename = 'Untitled'
+        self.buffer = GtkSource.Buffer(text=buf)
+        self.scroll = Gtk.ScrolledWindow()
+        self.editor = GtkSource.View(buffer=self.buffer)
+        self.scroll.add(self.editor)
+        self.box.pack_start(self.scroll, True, True, 0)
+
+        self.window.add(self.box)
+        self.window.show_all()
+
+class cmdBox(Gtk.Box):
+    def __init__(self):
+        Gtk.Box.__init__(self, orientation=Gtk.Orientation.HORIZONTAL)
+        
+        self.entry = Gtk.Entry()
+        self.pack_start(self.entry, True, True, 0)
+
+        # button text changes, gives feedback to user about current command
+        self.xec = Gtk.Button(label='verb')
+        self.pack_start(self.xec, False, False, 0)
+
+if __name__ == '__main__':
+    try:
+        filename = sys.argv[1]
+    except IndexError:
+        filename = ''
+    Wand(filename)
+    Gtk.main()
