ea80ade1d26c1a5564d8f7ad94b7483bce7abbff
andygoblins <thisismyfriendsemailaddress@gmail.com>
2015-07-10 14:38:59 -0500

    s/gtk/tkinter
    GTK does too much for me to make a working prototype. Example:
    Gtk.TextView has a right-click context menu, but I want to have a
    custom right-click menu that works completely different than
    standard context menus. I don't want to waste time figuring out
    how to hack the TextView class to disable this functionality.

--- wand.py
+++ wand.py
@@ -1,78 +1,36 @@
 #!/usr/bin/env python
 
 import sys
-from gi.repository import Gtk, GtkSource, GObject
+from tkinter import *
+from tkinter import ttk
 
-class Wand():
+class Wand(ttk.Frame):
     def __init__(self, filename):
-        self.window = Gtk.Window()
-        self.window.connect('delete-event', Gtk.main_quit)
-
-        self.filename = filename
+        ttk.Frame.__init__(self, master=None)
         try:
             with open(filename, 'r') as file:
                 buf = file.read()
+                self.master.title(filename)
         except FileNotFoundError:
             buf = ''
-            filename = 'Untitled'
-        self.buffer = GtkSource.Buffer(text=buf)
-        self.scroll = Gtk.ScrolledWindow()
-        self.editor = GtkSource.View(buffer=self.buffer)
-        self.scroll.add(self.editor)
+            self.master.title('Untitled')
 
-        self.box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
-        self.cmd = cmdBox(self.editor)
-        self.box.pack_start(self.cmd, False, False, 0)
-        self.box.pack_start(self.scroll, True, True, 0)
-        self.window.add(self.box)
-
-        self.window.show_all()
-
-class CmdBox(Gtk.Box):
-    def __init__(self, sourceView):
-        self.sourceView = sourceView
-        Gtk.Box.__init__(self, orientation=Gtk.Orientation.HORIZONTAL)
+        self.master.columnconfigure(0, weight=1)
+        self.master.rowconfigure(0, weight=1)
+            
+        self.grid(sticky=N+S+E+W)
+        self.columnconfigure(0, weight=1)
+        self.rowconfigure(0, weight=1)
         
-        self.entry = Gtk.Entry()
-        self.pack_start(self.entry, True, True, 0)
-
-        # button text changes, gives feedback to user about current command
-        self.xec = Gtk.Button(label='verb')
-        self.pack_start(self.xec, False, False, 0)
-
-        self.entry.connect('insert-at-cursor', self.parseCmd)
-        self.entry.connect('delete-from-cursor', self.parseCmd)
-
-    def parseCmd(self, *args):
-        if self.cmd:
-            #self.cmd = Cmd(text)
-            pass
-        else:
-            #self.cmd.setCmdTxt(text)
-            pass
-        print(self.entry.get_text())
-
-class Cmd():
-    def __init__(self, cmdtxt):
-        self.next = None
-        self.desc = ''
-        self._parse(cmdtxt)
-
-    def _parse(txt):
-        #sets cmd description
-        pass
-
-    def ok():
-        #return true if cmd is valid
-        pass
-
-    def setCmdTxt(txt):
-        self._parse(txt)
+        self.editor = Text(self)
+        self.editor.insert(0.0, buf)
+        self.editor.grid(row=0, column=0, sticky=N+S+E+W)
 
 if __name__ == '__main__':
     try:
         filename = sys.argv[1]
     except IndexError:
         filename = ''
-    Wand(filename)
-    Gtk.main()
+
+    wand = Wand(filename)
+    wand.mainloop()
