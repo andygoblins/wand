cd42951ed9ebf4b16d0cd9e9131532933dce6c2b
andygoblins <thisismyfriendsemailaddress@gmail.com>
2015-07-11 15:17:09 -0500

    scrollbar

--- wand.py
+++ wand.py
@@ -19,7 +19,11 @@ class Wand(ttk.Frame):
         
         self.editor = Text(self)
         self.editor.insert(0.0, buf)
-        self.editor.pack(fill=BOTH, expand=1)
+        self.editor.pack(side=LEFT, fill=BOTH, expand=1)
+        
+        self.scrollY = ttk.Scrollbar(self, orient=VERTICAL, command=self.editor.yview)
+        self.scrollY.pack(side=RIGHT, fill=Y)
+        self.editor['yscrollcommand'] = self.scrollY.set
 
 if __name__ == '__main__':
     try:
