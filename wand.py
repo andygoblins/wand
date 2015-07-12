9624f772b0e544000a9a5d09998fe00161b53085
andygoblins <thisismyfriendsemailaddress@gmail.com>
2015-07-12 14:12:22 -0500

    beginnings of a sam-like menu

--- wand.py
+++ wand.py
@@ -24,6 +24,34 @@ class Wand(ttk.Frame):
         self.scrollY = ttk.Scrollbar(self, orient=VERTICAL, command=self.editor.yview)
         self.scrollY.pack(side=RIGHT, fill=Y)
         self.editor['yscrollcommand'] = self.scrollY.set
+        
+        self.menu = SamMenu(self.master, '<3>', [('find', self.find),('copy', self.copy),('find', self.find)], 1)
+
+    def find(self):
+        print('find')
+        
+    def copy(self):
+        print('copy')
+        
+class SamMenu(Menu):
+    def __init__(self, root, button, items, primary):
+        Menu.__init__(self, root, tearoff=0)
+        self.primary = primary
+        for i in items:
+            self.add_command(label=i[0], command=i[1])
+        root.bind(button, self.popup)
+        #TODO need to make menu options still active even if mouse moves past bounds.
+        #self.bind('<Leave>', lambda e: "break")
+        #self.bind('<Enter>', lambda e: "break")
+        
+    def popup(self, event):
+        # 5 is magic. It might only look good with default font size...
+        yoff = self.yposition(self.primary) + 5
+        xoff = self.winfo_reqwidth() // 2
+        self.post(event.x_root - xoff, event.y_root - yoff)
+        # Need to grab pointer for menu to work right. Parent widget has 
+        # pointer grab after button press for drag events.
+        self.grab_set()
 
 if __name__ == '__main__':
     try:
