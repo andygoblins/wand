74b4467a36203fba8a983aa16b315cbc9f4799cf
andygoblins <thisismyfriendsemailaddress@gmail.com>
2015-07-18 19:46:15 -0500

    Separate app class from editor window.
    ...And insert code that fixes a bug in the current version of
    tkinter.

--- wand.py
+++ wand.py
@@ -4,7 +4,20 @@ import sys
 from tkinter import *
 from tkinter import ttk
 
-class Wand(ttk.Frame):
+class Wand():
+    def __init__(self, filename):
+        self.editors = {}
+        # TODO load config file here
+        self.open(filename)
+    def open(self, filename):
+        if filename in self.editors:
+            #open a peer
+            ed = self.editors[filename]
+            ed.peer_create(
+        else:
+            #open a new file
+
+class Editor(ttk.Frame):
     def __init__(self, filename):
         ttk.Frame.__init__(self, master=None)
         try:
@@ -27,25 +40,59 @@ class Wand(ttk.Frame):
         
         self.menu = SamMenu(self.master, '<3>', [('find', self.find),('copy', self.copy),('find', self.find)], 1)
 
+        #TODO
+        self.bind('<Destroy>' self.destroy)
+    def destroy(self):
+        #TODO unregister with Wand (Wand keeps track of editor peers).
+        pass
     def find(self):
         print('find')
-        
     def copy(self):
         print('copy')
+
+class TextHack(Text):
+    """Same as regular Text, but implements fix from
+    https://bugs.python.org/issue17945"""
+
+    def peer_create(self, newPathName=None, cnf={}, **kw): # new in Tk 8.5
+            """Creates a peer text widget and any optional standard
+            configuration options. By default the peer will have the same
+            start and and end line as the parent widget, but these can be
+            overriden with the standard configuration options."""
+            if master is None:
+                master = self.master
+            if 'name' not in cnf:
+                peername = '%s%s%d' % (self._name, '_peer', next(self.peer_count))
+            else:
+                peername = cnf['name']
+                cnf.pop('name')
+            if master == '.':
+                peerpath = '.' + peername
+            else:
+                peerpath = master._w + '.' + peername
+            self.tk.call(self._w, 'peer', 'create', peerpath,
+                     *self._options(cnf, kw))
+            peer = _textpeer(master, peername)
+            return peer
+
+class _textpeer(Text):
+    """Internal class used to represent a text peer. Part of fix for
+    https://bugs.python.org/issue17945"""
+    def __init__(self, master, name):
+        BaseWidget._setup(self, master, {'name': name})
         
 class SamMenu(Menu):
-    '''SamMenu behaves like the mouse menus from the text editor sam.
-    The menu stays open only while the mouse button is held down, and selects
-    whatever is under the mouse when the mouse button is released. The "primary"
-    menu entry is directly under the mouse when the menu appears.
-    '''
+    """SamMenu behaves like the mouse menus from the text editor sam.
+    The menu stays open only while the mouse button is held down, and
+    selects whatever is under the mouse when the mouse button is
+    released. The "primary" menu entry is directly under the mouse when
+    the menu appears."""
     def __init__(self, root, button, items, primary):
         Menu.__init__(self, root, tearoff=0)
         self.primary = primary
         for i in items:
             self.add_command(label=i[0], command=i[1])
         root.bind(button, self.popup)
-        
     def popup(self, event):
         # 5 is magic. It might only look good with default font size...
         yoff = self.yposition(self.primary) + 5
