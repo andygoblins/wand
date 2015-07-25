96a6af363392d7b68d2944ad91933618c20331ab
andygoblins <thisismyfriendsemailaddress@gmail.com>
2015-07-25 13:11:02 -0500

    run before I walk.
    Tkinter's peering is messy. This is just a prototype, so it's not
    worth dealing with.

--- wand.py
+++ wand.py
@@ -1,39 +1,33 @@
 #!/usr/bin/env python
 
 import sys
-import itertools
 from tkinter import *
 from tkinter import ttk
 
 class Wand():
-    def __init__(self, filename):
-        self.editors = {}
+    """Main application class. Manages windows/frames"""
+    def __init__(self, master, filename):
+        self.master = master
         # TODO load config file here
+        # TODO use full filename, not relative
         self.open(filename)
     def open(self, filename):
-        if filename in self.editors:
-            ed = self.editors[filename][0]
-            n = Editor(filename, peer=ed)
-            self.editors[filename].append(n)
-        else:
-            n = Editor(filename)
-            self.editors[filename] = [n]
+        win = Toplevel(self.master)
+        Editor(win, filename)
 
 class Editor(ttk.Frame):
-    def __init__(self, filename, master=None, peer=None):
+    """An editor window."""
+    def __init__(self, master, filename):
         ttk.Frame.__init__(self, master=master)
-        if peer:
-            self.editor = peer.editor.peer_create(master)
-        else:
-            try:
-                with open(filename, 'r') as file:
-                    buf = file.read()
-                    self.master.title(filename)
-            except FileNotFoundError:
+        try:
+            with open(filename, 'r') as file:
+                buf = file.read()
+                self.master.title(filename)
+        except FileNotFoundError:
                 buf = ''
                 self.master.title('Untitled')
-            self.editor = TextHack(self)
-            self.editor.insert(0.0, buf)
+        self.editor = Text(self)
+        self.editor.insert(0.0, buf)
 
         self.editor.pack(side=LEFT, fill=BOTH, expand=1)  
         self.pack(fill=BOTH, expand=1)
@@ -45,51 +39,10 @@ class Editor(ttk.Frame):
         self.menu = SamMenu(self.master, '<3>', [('find', self.find),
                                                  ('copy', self.copy),
                                                  ('find', self.find)], 1)
-        self.bind('<Destroy>', self.close)
-    def close(self, event):
-        #TODO unregister with Wand (Wand keeps track of editor peers).
-        pass
     def find(self):
         print('find')
     def copy(self):
         print('copy')
-
-class TextHack(Text):
-    """Same as regular Text, but implements fix from
-    https://bugs.python.org/issue17945"""
-
-    peer_count = itertools.count(1)
-    
-    def __init__(self, master=None, **kw):
-        Text.__init__(self, master, **kw)
-
-    def peer_create(self, master=None, cnf={}, **kw): # new in Tk 8.5
-            """Creates a peer text widget and any optional standard
-            configuration options. By default the peer will have the same
-            start and and end line as the parent widget, but these can be
-            overriden with the standard configuration options."""
-            if master is None:
-                master = self.master
-            if 'name' not in cnf:
-                peername = '%s%s%d' % (self._name, '_peer',
-                                       next(self.peer_count))
-            else:
-                peername = cnf['name']
-                cnf.pop('name')
-            if master == '.':
-                peerpath = '.' + peername
-            else:
-                peerpath = master._w + '.' + peername
-            self.tk.call(self._w, 'peer', 'create', peerpath,
-                     *self._options(cnf, kw))
-            peer = _textpeer(master, peername)
-            return peer
-
-class _textpeer(Text):
-    """Internal class used to represent a text peer. Part of fix for
-    https://bugs.python.org/issue17945"""
-    def __init__(self, master, name):
-        BaseWidget._setup(self, master, {'name': name})
         
 class SamMenu(Menu):
     """SamMenu behaves like the mouse menus from the text editor sam.
@@ -118,6 +71,7 @@ if __name__ == '__main__':
     except IndexError:
         filename = ''
 
-    wand = Wand(filename)
-    wand.open(filename)
-    wand.mainloop()
+    root = Tk()
+    root.withdraw()
+    wand = Wand(root, filename)
+    root.mainloop() 
