56c3ed8b804f9286ec53e8a9cc47f6d0502c9b57
andygoblins <thisismyfriendsemailaddress@gmail.com>
2015-07-26 15:56:41 -0500

    URL bar
    why shouldn't text editing look like web browsing?

--- wand.py
+++ wand.py
@@ -1,36 +1,64 @@
 #!/usr/bin/env python
 
 import sys
+import os
 from tkinter import *
 from tkinter import ttk
 
-class Wand():
+class Wand(ttk.Frame):
     """Main application class. Manages windows/frames"""
     def __init__(self, master, filename):
+        ttk.Frame.__init__(self, master=master)
+        master.withdraw()
         self.master = master
+        self.wincount = 0
+        self.bind_class('Toplevel', '<Destroy>', self.countwins)
         # TODO load config file here
-        # TODO use full filename, not relative
+        filename = os.path.realpath(filename) if filename else ''
         self.open(filename)
     def open(self, filename):
         win = Toplevel(self.master)
         Editor(win, filename)
+        self.wincount += 1
+    def countwins(self, event):
+        if self.wincount > 1:
+            self.wincount -= 1
+        else:
+            self.master.destroy()
 
 class Editor(ttk.Frame):
     """An editor window."""
     def __init__(self, master, filename):
         ttk.Frame.__init__(self, master=master)
+        self.pack(fill=BOTH, expand=1)
+        self.tk_focusFollowsMouse()
         try:
             with open(filename, 'r') as file:
                 buf = file.read()
-                self.master.title(filename)
+            self.filename = filename
         except FileNotFoundError:
-                buf = ''
-                self.master.title('Untitled')
+            #TODO indicate to user this is a new file?
+            buf = ''
+            self.filename = ''
+        self.master.title(self.filename)
+        self.build_widgets(buf)
+    def build_widgets(self, buf):
+        # controls frame has URL bar and basic buttons,
+        # such as load/refresh, undo, redo, save,
+        # and close.
+        self.controls = ttk.Frame(self)
+        self.controls.pack(side=TOP, fill=BOTH, expand=1)
+        self.urlbar = ttk.Entry(self.controls)
+        self.urlbar.insert(0, self.filename)
+        self.urlbar.pack(side=LEFT, fill=BOTH, expand=1)
+        r = ttk.Button(self.controls, text="Load", command=self.refresh)
+        r.pack(side=RIGHT)
+        
+        # below the controls frame is the main editor window
         self.editor = Text(self)
+        self.editor['insertunfocussed'] = 'solid' #don't ever hide cursor
         self.editor.insert(0.0, buf)
-
-        self.editor.pack(side=LEFT, fill=BOTH, expand=1)  
-        self.pack(fill=BOTH, expand=1)
+        self.editor.pack(side=LEFT, fill=BOTH, expand=1)
         self.scrollY = ttk.Scrollbar(self, orient=VERTICAL,
                                      command=self.editor.yview)
         self.scrollY.pack(side=RIGHT, fill=Y)
@@ -39,6 +67,8 @@ class Editor(ttk.Frame):
         self.menu = SamMenu(self.master, '<3>', [('find', self.find),
                                                  ('copy', self.copy),
                                                  ('find', self.find)], 1)
+    def refresh(self):
+    	print('refresh')
     def find(self):
         print('find')
     def copy(self):
@@ -72,6 +102,5 @@ if __name__ == '__main__':
         filename = ''
 
     root = Tk()
-    root.withdraw()
     wand = Wand(root, filename)
-    root.mainloop() 
+    root.mainloop()
